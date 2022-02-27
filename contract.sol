b1012167d358004913f4d26d58fa14e91d95a3ad
pedroafleite <pedroaug.fl@gmail.com>
2022-02-27 10:13:24 -0300

    fix contract

--- contract.sol
+++ contract.sol
@@ -1,9 +1,10 @@
-// SPDC-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts@4.4.1/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.4.1/access/Ownable.sol";
-import "@openzeppelin/contracts@4.4.1/utils.Counters.sol";
+import "@openzeppelin/contracts@4.4.1/utils/Counters.sol";
 import "@openzeppelin/contracts@4.4.1/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts@4.4.1/utils/cryptography/draft-EIP712.sol";
 
@@ -35,12 +36,12 @@ contract MyToken is ERC721, EIP712, Ownable {
         return ECDSA.recover(digest, signature);
     }
 
-    function _hash(uint256 id, string message) internal view returns (bytes32) {
+    function _hash(uint256 id, string memory message) internal view returns (bytes32) {
         return _hashTypedDataV4(keccak256(abi.encode(
-            keccak256("Web3Struct(uint256 id,string message"),
+            keccak256("Web3Struct(uint256 id,string message)"),
             id,
             keccak256(bytes(message))
             // keccak256(bytes(STRING)) for string
-        )))
+        )));
     }
 }
